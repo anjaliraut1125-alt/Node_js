9411fba1926ff88b5312c5b1bc75532f7754606d
anjaliraut1125-alt <anjaliraut1125@gmail.com>
2026-05-16 12:01:31 +0530

    routing

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const http = require('http');
 
+// const server = http.createServer((req , res)=>{
+//     res.write("Welcome to Node Js Server!!");
+//     res.end();
+// });
+
 const server = http.createServer((req , res)=>{
-    res.write("Welcome to Node Js Server!!");
-    res.end();
-});
+    if (req.url === '/'){
+        res.end("Home Page");
+
+    } else if (req.url === '/about'){
+        res.end("About US");
+
+    }else {
+        res.end("404 Page Not Found");
+    }
+})
 
 server.listen(3000,()=>{
     console.log("Server is running on port 3000");

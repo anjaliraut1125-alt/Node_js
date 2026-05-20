218c74a84394f368646285fcc4ded4f6774fe1d6
anjaliraut1125-alt <anjaliraut1125@gmail.com>
2026-05-20 10:13:19 +0530

    pending work

--- express.js
+++ express.js
@@ -1,11 +1,24 @@
 const express = require('express');
 
+// const app = express();
+
+// app.get('/', (req , res)=> {
+//     res.send("Hello World!");
+// });
+
+
+// app.listen(5000,()=>{
+//     console.log("Server is running on port 5000");
+// });
+
+
+
+// types of routes
 const app = express();
 
-app.get('/', (req , res)=> {
-    res.send("Hello World!");
+app.get("/students", (req , res)=>{
+    res.send("Student List");
 });
-
 
 app.listen(5000,()=>{
     console.log("Server is running on port 5000");

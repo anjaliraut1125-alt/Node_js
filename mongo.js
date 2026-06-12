a0d67e8e663d66c8bac05405d63cbbad5004f019
anjaliraut1125-alt <anjaliraut1125@gmail.com>
2026-06-12 12:08:13 +0530

    postman

---
+++ mongo.js
@@ -0,0 +1,56 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]); 
+
+
+const express = require("express");
+const mongoose =  require("mongoose");
+
+const app = express();
+
+mongoose.connect(
+    "mongodb+srv://anjaliraut1125_db_user:anjali%402002@cluster0.7jn8k9z.mongodb.net/?appName=Cluster0"
+    
+).then(()=>{
+    console.log("Mongodb Connected Suceessfully!");
+}).catch((err)=>{
+    console.log("Connection Failed!");
+    // console.log(err.message);
+    console.error(err);
+});
+
+app.use(express.json())
+const UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+
+    email:{
+        type:String,
+        required: true,
+        unique: true
+    },
+    
+    age:{
+        type:Number,
+        required:true
+    }
+})
+
+const User = mongoose.model("User", UserSchema);
+
+app.post("/users" , async(req , res)=>{
+    try{
+        const user = await User.create(req.body);
+        res.status(201).json(user);
+    }
+    catch(err){
+        res.status(500).json(err.message);
+    }
+})
+
+
+
+app.listen(8000, ()=>{
+    console.log("Server is running on port number 8000.");
+});
